941b8501cc9031b1846654c76657435f656a2ece
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2022-06-25 22:26:52 -0700

    update seg size

--- src/cache.rs
+++ src/cache.rs
@@ -9,7 +9,7 @@ use spin::{rwlock::RwLockWriteGuard, RwLockReadGuard};
 use std::mem::ManuallyDrop;
 
 /// Jasmine manages memory at the granularity of segments.
-pub const SEGMENT_SIZE: usize = 4 * 1024; // 4KB
+pub const SEGMENT_SIZE: usize = 2 * 1024 * 1024; // 2MB
 
 const SEGMENT_ALIGN: usize = 0x0fff;
 
